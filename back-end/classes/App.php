b79b44321ccfc95e4f90615920b73a6c0ced07d3
Lewis Newton <lewisknewton@hotmail.com>
2020-02-14 12:57:40 +0000

    add helper scripts
    - used to start app processes

---
+++ back-end/classes/App.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Represents generally the application's running as a whole.
+ */
+class App {
+  public $title = 'Appointment Booking System';
+  private $__db;
+
+  /**
+   * Creates a new App using the given configuration parameters.
+   * The configuration should, at minimum, include parameters for database creation.
+   */
+  public function __construct() {
+    $this->__start_database();
+  }
+
+  /**
+   * Creates a new Database using the stored database configuration parameters.
+   *
+   * @return void
+   */
+  private function __start_database() {
+    $this->__db = new Database();
+  }
+
+  /**
+   * Redirects the user to the given path.
+   *
+   * @param $location
+   * @return void
+   */
+  public function redirectTo($location) {
+    header("Location: $location");
+  }
---
+++ back-end/init.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Autoloads classes in /back-end/classes/.
+ *
+ * @param $class_ref
+ * @return void
+ */
+function autoload_classes($class_ref) {
+  // Isolate class name from namespace
+  $class_ref_parts = explode('\\', $class_ref);
+  $class_name = end($class_ref_parts);
+
+  $class_url = dirname(__FILE__) . "/classes/$class_name.php";
+  require_once $class_url;
+}
+
+spl_autoload_register('autoload_classes');
+
+// @codeCoverageIgnoreStart
+// Instantiate the application
+$GLOBALS['app'] = new App();
+// @codeCoverageIgnoreEnd
