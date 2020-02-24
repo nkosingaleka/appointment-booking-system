26096a14349edf60dff2c3cc3af60cfc2f3a6feb
Lewis Newton <lewisknewton@hotmail.com>
2020-02-24 09:27:35 +0000

    update test data to include valid email addresses
    - removes hyphen from previous examples

--- back-end/database/test_data.sql
+++ back-end/database/test_data.sql
@@ -9,21 +9,21 @@ INSERT INTO `role` (`description`) VALUES ('patient');
 
 -- Account: Different accounts for each role (password = 'test123')
 INSERT INTO `account` (`id`, `email`, `password`, `role_id`, `verified`) 
-  VALUES ('as-5e4685e3c899f1.04316598', 'as-1@test.com', '$2y$10$vLmKOHyBeRKCaXap7Xt5nuxc5XoZ1hZjmlIMl/GwK2zL4D2MHpefK', 1, true);
+  VALUES ('as-5e4685e3c899f1.04316598', 'as1@test.com', '$2y$10$vLmKOHyBeRKCaXap7Xt5nuxc5XoZ1hZjmlIMl/GwK2zL4D2MHpefK', 1, true);
 INSERT INTO `account` (`id`, `email`, `password`, `role_id`, `verified`) 
-  VALUES ('as-5e46873222d123.80231466', 'as-2@test.com', '$2y$10$vLmKOHyBeRKCaXap7Xt5nuxc5XoZ1hZjmlIMl/GwK2zL4D2MHpefK', 1, true);
+  VALUES ('as-5e46873222d123.80231466', 'as2@test.com', '$2y$10$vLmKOHyBeRKCaXap7Xt5nuxc5XoZ1hZjmlIMl/GwK2zL4D2MHpefK', 1, true);
 INSERT INTO `account` (`id`, `email`, `password`, `role_id`, `verified`) 
-  VALUES ('ms-5e4685f6e57722.33264537', 'ms-1@test.com', '$2y$10$vLmKOHyBeRKCaXap7Xt5nuxc5XoZ1hZjmlIMl/GwK2zL4D2MHpefK', 2, true);
+  VALUES ('ms-5e4685f6e57722.33264537', 'ms1@test.com', '$2y$10$vLmKOHyBeRKCaXap7Xt5nuxc5XoZ1hZjmlIMl/GwK2zL4D2MHpefK', 2, true);
 INSERT INTO `account` (`id`, `email`, `password`, `role_id`, `verified`) 
-  VALUES ('ms-5e46874b062833.95604018', 'ms-2@test.com', '$2y$10$vLmKOHyBeRKCaXap7Xt5nuxc5XoZ1hZjmlIMl/GwK2zL4D2MHpefK', 2, true);
+  VALUES ('ms-5e46874b062833.95604018', 'ms2@test.com', '$2y$10$vLmKOHyBeRKCaXap7Xt5nuxc5XoZ1hZjmlIMl/GwK2zL4D2MHpefK', 2, true);
 INSERT INTO `account` (`id`, `email`, `password`, `role_id`, `verified`) 
-  VALUES ('pa-5e4686095092e0.76300630', 'pa-1@test.com', '$2y$10$vLmKOHyBeRKCaXap7Xt5nuxc5XoZ1hZjmlIMl/GwK2zL4D2MHpefK', 3, true);
+  VALUES ('pa-5e4686095092e0.76300630', 'pa1@test.com', '$2y$10$vLmKOHyBeRKCaXap7Xt5nuxc5XoZ1hZjmlIMl/GwK2zL4D2MHpefK', 3, true);
 INSERT INTO `account` (`id`, `email`, `password`, `role_id`, `verified`) 
-  VALUES ('pa-5e46875705e162.50418870', 'pa-2@test.com', '$2y$10$vLmKOHyBeRKCaXap7Xt5nuxc5XoZ1hZjmlIMl/GwK2zL4D2MHpefK', 3, true);
+  VALUES ('pa-5e46875705e162.50418870', 'pa2@test.com', '$2y$10$vLmKOHyBeRKCaXap7Xt5nuxc5XoZ1hZjmlIMl/GwK2zL4D2MHpefK', 3, true);
 INSERT INTO `account` (`id`, `email`, `password`, `role_id`, `verified`) 
-  VALUES ('pa-5e46876f3d9332.22430483', 'pa-3@test.com', '$2y$10$vLmKOHyBeRKCaXap7Xt5nuxc5XoZ1hZjmlIMl/GwK2zL4D2MHpefK', 3, false);
+  VALUES ('pa-5e46876f3d9332.22430483', 'pa3@test.com', '$2y$10$vLmKOHyBeRKCaXap7Xt5nuxc5XoZ1hZjmlIMl/GwK2zL4D2MHpefK', 3, false);
 INSERT INTO `account` (`id`, `email`, `password`, `role_id`, `verified`) 
-  VALUES ('pa-5e468774d36a40.10626920', 'pa-4@test.com', '$2y$10$vLmKOHyBeRKCaXap7Xt5nuxc5XoZ1hZjmlIMl/GwK2zL4D2MHpefK', 3, false);
+  VALUES ('pa-5e468774d36a40.10626920', 'pa4@test.com', '$2y$10$vLmKOHyBeRKCaXap7Xt5nuxc5XoZ1hZjmlIMl/GwK2zL4D2MHpefK', 3, false);
 
 -- Staff: Different accounts for both roles
 INSERT INTO `staff` (`id`, `title`, `forename`, `surname`, `sex`, `job_title`, `facility_id`)
