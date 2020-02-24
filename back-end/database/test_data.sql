dd8b847baeeb21d903f76ab8b97dc7a14f3402c4
Lewis Newton <lewisknewton@hotmail.com>
2020-02-24 11:02:56 +0000

    change comment format in SQL
    - necessary for solving Travis CI MySQL failure

--- back-end/database/test_data.sql
+++ back-end/database/test_data.sql
@@ -1,13 +1,13 @@
--- Facility: Example facility
+/* Facility: Example facility */
 INSERT INTO `facility` (`id`, `name`, `building_name`, `building_no`, `street`, `city`, `county`, `postcode`, `tel_no`) 
   VALUES ('5e4a83211ffd59.63214101', 'Derby Road Group Practice', null, '27-29', 'Derby Rd', 'Portsmouth', 'Hampshire', 'PO2 8HW', '+442392009265');
 
--- Role: 3 Default roles
+/* Role: 3 Default roles */
 INSERT INTO `role` (`description`) VALUES ('administrative_staff');
 INSERT INTO `role` (`description`) VALUES ('medical_staff');
 INSERT INTO `role` (`description`) VALUES ('patient');
 
--- Account: Different accounts for each role (password = 'test123')
+/* Account: Different accounts for each role (password = 'test123') */
 INSERT INTO `account` (`id`, `email`, `password`, `role_id`, `verified`) 
   VALUES ('as-5e4685e3c899f1.04316598', 'as1@test.com', '$2y$10$vLmKOHyBeRKCaXap7Xt5nuxc5XoZ1hZjmlIMl/GwK2zL4D2MHpefK', 1, true);
 INSERT INTO `account` (`id`, `email`, `password`, `role_id`, `verified`) 
@@ -25,7 +25,7 @@ INSERT INTO `account` (`id`, `email`, `password`, `role_id`, `verified`)
 INSERT INTO `account` (`id`, `email`, `password`, `role_id`, `verified`) 
   VALUES ('pa-5e468774d36a40.10626920', 'pa4@test.com', '$2y$10$vLmKOHyBeRKCaXap7Xt5nuxc5XoZ1hZjmlIMl/GwK2zL4D2MHpefK', 3, false);
 
--- Staff: Different accounts for both roles
+/* Staff: Different accounts for both roles */
 INSERT INTO `staff` (`id`, `title`, `forename`, `surname`, `sex`, `job_title`, `facility_id`)
   VALUES ('as-5e4685e3c899f1.04316598', 'Mr', 'Matthew', 'Johnson', 'M', 'Receptionist', '5e4a83211ffd59.63214101');
 INSERT INTO `staff` (`id`, `title`, `forename`, `surname`, `sex`, `job_title`, `facility_id`)
@@ -35,7 +35,7 @@ INSERT INTO `staff` (`id`, `title`, `forename`, `surname`, `sex`, `job_title`, `
 INSERT INTO `staff` (`id`, `title`, `forename`, `surname`, `sex`, `job_title`, `facility_id`)
   VALUES ('ms-5e46874b062833.95604018', 'Dr', 'Margaret', 'Christchurch', 'F', 'General Practitioner', '5e4a83211ffd59.63214101');
 
--- Next of Kin: Records for each patient record
+/* Next of Kin: Records for each patient record */
 INSERT INTO `next_of_kin` (`id`, `relationship`, `title`, `forename`, `surname`, `house_name`, `house_no`, `street`, `city`, `county`, `postcode`, `tel_no`, `mob_no`)
   VALUES ('nk-5e4693079a5e18.63580261', 'Father', 'Mr', 'Nerti', 'Philson', null, '7', 'London Road', 'Southampton', 'Hampshire', 'SO15 2AE', '+447691014037', '+448865738896');
 INSERT INTO `next_of_kin` (`id`, `relationship`, `title`, `forename`, `surname`, `house_name`, `house_no`, `street`, `city`, `county`, `postcode`, `tel_no`, `mob_no`)
@@ -45,7 +45,7 @@ INSERT INTO `next_of_kin` (`id`, `relationship`, `title`, `forename`, `surname`,
 INSERT INTO `next_of_kin` (`id`, `relationship`, `title`, `forename`, `surname`, `house_name`, `house_no`, `street`, `city`, `county`, `postcode`, `tel_no`, `mob_no`)
   VALUES ('nk-5e4694507dc976.27152557', 'Son', 'Dr', 'Kirby', 'Botler', 'The Willows', null, 'Wilson St', 'Gosport', 'Hampshire', 'PO12 4PQ', '+449685024267', '+448489534531');
 
--- Patient: Different records for each patient account
+/* Patient: Different records for each patient account */
 INSERT INTO `patient` (`id`, `title`, `forename`, `surname`, `sex`, `date_of_birth`, `house_name`, `house_no`, `street`, `city`, `county`, `postcode`, `tel_no`, `mob_no`, `next_of_kin`, `NHS_no`, `HC_no`)
   VALUES ('pa-5e4686095092e0.76300630', 'Mr', 'David', 'Bowden', 'M', '1990-01-26', null, '12', 'Meyrick Road', 'Portsmouth', 'Hampshire', 'PO2 RJF', '+447691014037', '+448865738896', 'nk-5e4693079a5e18.63580261', '4562594365', null);
 INSERT INTO `patient` (`id`, `title`, `forename`, `surname`, `sex`, `date_of_birth`, `house_name`, `house_no`, `street`, `city`, `county`, `postcode`, `tel_no`, `mob_no`, `next_of_kin`, `NHS_no`, `HC_no`)
