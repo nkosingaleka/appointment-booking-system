2144cbd5bb9691064d18e3ffeced875177bd496d
Alexander-Portland <up896020@myport.ac.uk>
2020-02-24 14:07:56 +0000

    adds a description on the verification page

--- front-end/Verification.php
+++ front-end/Verification.php
@@ -5,7 +5,9 @@
 </head>
 
 <body>
-
+<h1>Verification page</h1>
+<p>This function will allow the administrator to view all of the requests</p>
+<p>They will then select the account they want to confirm and it will then change the account detail so that it can be logged into</p>
 </body>
 
 </html>
