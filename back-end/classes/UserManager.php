7303efc6d17f3b36fc7af74ae47a0c97da8c841e
Lewis Newton <lewisknewton@hotmail.com>
2020-03-04 18:53:55 +0000

    clean up email code

--- back-end/classes/UserManager.php
+++ back-end/classes/UserManager.php
@@ -1,14 +1,8 @@
 <?php
 
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\SMTP;
+
 require dirname(__FILE__) . '/../vendor/autoload.php';
-
-
-
-
-
 
 /**
  * Represents the component for handling user-related actions.
@@ -353,44 +347,34 @@ class UserManager {
   }
 
   public static function receiveEmail() {
-    
-     
-
     $mail = new PHPMailer;
 
-    $mail->SMTPDebug = 2;                              // Enable verbose debug output
+    // Enable verbose error outputs
+    $mail->SMTPDebug = 2;
 
-    $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'jakelove15s@gmail.com';                 // SMTP username
-    $mail->Password = '78RanGers01FuckOfF678';                           // SMTP password
-    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 465;                                    // TCP port to connect to
+    $mail->IsSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'EMAIL';
+    $mail->Password = 'PASSWORD';
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port = 465;
+    $mail->IsHTML(true);
 
-    $mail->setFrom('jakelove15s@gmail.com', 'Jake');
-
-    $mail->addAddress('up734426@myport.ac.uk', 'Patient');     // Add a recipient
-
-    // $mail->addReplyTo('info@example.com', 'Information');
-    // $mail->addCC('cc@example.com');
-    // $mail->addBCC('bcc@example.com');
-
-    // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-    // $mail->isHTML(true); // Set email format to HTML
-
+    // Add sender
+    $mail->SetFrom('EMAIL', 'MEDICAL_FACILITY');
   
+    // Add recipient
+    $mail->AddAddress('PATIENT_EMAIL', 'PATIENT_NAME');
 
     $mail->Subject = 'This is a test email';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->Body = 'This is the HTML message body <b>in bold!</b>';
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-    if(!$mail->send()) {
+    if (!$mail->Send()) {
         echo 'Message could not be sent.';
         echo 'Mailer Error: ' . $mail->ErrorInfo;
-    } 
-    else {
+    } else {
         echo 'Message has been sent';
     }
   }
