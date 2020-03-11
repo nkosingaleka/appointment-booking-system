2dc608c09468144c3fb79d53ccb7716e16a0c661
Lewis Newton <lewisknewton@hotmail.com>
2020-03-11 09:21:26 +0000

    remove duplicate create table statement

--- back-end/database/database.sql
+++ back-end/database/database.sql
@@ -176,12 +176,3 @@ CREATE TABLE IF NOT EXISTS `attendance` (
   FOREIGN KEY (`appointment_id`) REFERENCES `appointment` (`request_id`),
   FOREIGN KEY (`staff_id`) REFERENCES `staff` (`id`)
 );
-
-CREATE TABLE IF NOT EXISTS `attendance` (
-  `id` VARCHAR(36),
-  `appointment_id` VARCHAR(36),
-  `staff_id` VARCHAR(36),
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`appointment_id`) REFERENCES `appointment` (`id`),
-  FOREIGN KEY (`staff_id`) REFERENCES `staff` (`id`)
-);
