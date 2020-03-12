de01ef0b3f458737589d1eb0c3d352d98dca35b4
Lewis Newton <lewisknewton@hotmail.com>
2020-03-12 13:36:13 +0000

    add hyperlink to the home page

--- back-end/includes/nav.inc.php
+++ back-end/includes/nav.inc.php
@@ -1,6 +1,6 @@
 <nav>
   <ul>
-    <li><a href="#">Home</a></li>
+    <li><a href="index.php">Home</a></li>
     <li><a href="#">Account</a></li>
     <li><a href="scripts/logout.php">Log out</a></li>
   </ul>
