e5976e50c167f4812e525396e8097b9728cd1867
Lewis Newton <lewisknewton@hotmail.com>
2020-03-22 19:25:28 +0000

    make presentation of patient details clearer
    - uses human-readable field names

--- front-end/patient-accounts.php
+++ front-end/patient-accounts.php
@@ -75,40 +75,122 @@ if (isset($_GET['query']) && !empty($_GET['query'])) {
 
     <section id="verified-patients">
       <h2>Verified Patients</h2>
+
       <?php if (count($verified_patients) > 0): ?>
         <?php foreach ($verified_patients as $patient): ?>
           <article>
             <ul>
-              <?php foreach (array_keys($patient) as $detail): ?>
-                <?php if (!empty($patient[$detail])): ?>
-                  <li><b><?=ucfirst($detail)?></b> <?=': ' . $patient[$detail]?></li>
-                <?php endif?>
-              <?php endforeach?>
+              <li>
+                <b>ID:</b> <?=$patient['id']?>
+              </li>
+              <li>
+                <b>Email address:</b> <?=$patient['email']?>
+              </li>
+              <li>
+                <b>Full name:</b> <?=$patient['title'] . ' ' . $patient['forename'] . ' ' . $patient['surname']?>
+              </li>
+              <li>
+                <b>Sex:</b> <?=$patient['sex']?>
+              </li>
+              <li>
+                <b>Date of birth:</b> <?=$patient['date_of_birth']?>
+              </li>
+              <li>
+                <b>Address:</b> <?=$patient['house_no'] ?? $patient['house_name'] . ' ' . $patient['street'] . ', ' . $patient['city'] . ', ' . $patient['county'] . ', ' . $patient['postcode']?>
+              </li>
+
+              <?php if (isset($patient['tel_no'])): ?>
+                <li>
+                  <b>Telephone Number:</b> <?=$patient['tel_no']?>
+                </li>
+              <?php endif?>
+
+              <?php if (isset($patient['mob_no'])): ?>
+                <li>
+                  <b>Mobile Number:</b> <?=$patient['mob_no']?>
+                </li>
+              <?php endif?>
+
+              <li>
+                <b>Next of kin ID:</b> <?=$patient['next_of_kin']?>
+              </li>
+
+              <?php if (isset($patient['NHS_no'])): ?>
+                <li>
+                  <b>NHS Number:</b> <?=$patient['NHS_no']?>
+                </li>
+              <?php endif?>
+
+              <?php if (isset($patient['HC_no'])): ?>
+                <li>
+                  <b>Health and Care Number:</b> <?=$patient['HC_no']?>
+                </li>
+              <?php endif?>
             </ul>
           </article>
         <?php endforeach?>
-        <?php else: ?>
+      <?php else: ?>
         <p>Sorry, no patients were found.</p>
       <?php endif?>
     </section>
 
     <section id="unverified-patients" class="hidden">
       <h2>Unverified Patients</h2>
+
       <?php if (count($unverified_patients) > 0): ?>
         <?php foreach ($unverified_patients as $patient): ?>
           <article>
             <ul>
-              <?php foreach (array_keys($patient) as $detail): ?>
-                <?php if (!empty($patient[$detail])): ?>
-                  <li><b><?=ucfirst($detail)?></b> <?=': ' . $patient[$detail]?></li>
-                <?php endif?>
-              <?php endforeach?>
-            </ul>
+              <li>
+                <b>ID:</b> <?=$patient['id']?>
+              </li>
+              <li>
+                <b>Email address:</b> <?=$patient['email']?>
+              </li>
+              <li>
+                <b>Full name:</b> <?=$patient['title'] . ' ' . $patient['forename'] . ' ' . $patient['surname']?>
+              </li>
+              <li>
+                <b>Sex:</b> <?=$patient['sex']?>
+              </li>
+              <li>
+                <b>Date of birth:</b> <?=$patient['date_of_birth']?>
+              </li>
+              <li>
+                <b>Address:</b> <?=$patient['house_no'] ?? $patient['house_name'] . ' ' . $patient['street'] . ', ' . $patient['city'] . ', ' . $patient['county'] . ', ' . $patient['postcode']?>
+              </li>
 
-            <a href="verify.php?id=<?=$patient['id']?>">Verify</a>
+              <?php if (isset($patient['tel_no'])): ?>
+                <li>
+                  <b>Telephone Number:</b> <?=$patient['tel_no']?>
+                </li>
+              <?php endif?>
+
+              <?php if (isset($patient['mob_no'])): ?>
+                <li>
+                  <b>Mobile Number:</b> <?=$patient['mob_no']?>
+                </li>
+              <?php endif?>
+
+              <li>
+                <b>Next of kin ID:</b> <?=$patient['next_of_kin']?>
+              </li>
+
+              <?php if (isset($patient['NHS_no'])): ?>
+                <li>
+                  <b>NHS Number:</b> <?=$patient['NHS_no']?>
+                </li>
+              <?php endif?>
+
+              <?php if (isset($patient['HC_no'])): ?>
+                <li>
+                  <b>Health and Care Number:</b> <?=$patient['HC_no']?>
+                </li>
+              <?php endif?>
+            </ul>
           </article>
         <?php endforeach?>
-        <?php else: ?>
+      <?php else: ?>
         <p>Sorry, no patients were found.</p>
       <?php endif?>
     </section>
