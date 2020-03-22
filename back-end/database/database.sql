d676109ca65987b3159e0db5462275e787600e5d
Lewis Newton <lewisknewton@hotmail.com>
2020-03-22 15:01:05 +0000

    add defaults for boolean attributes
    - covers reserved slots and cancellations

--- back-end/database/database.sql
+++ back-end/database/database.sql
@@ -107,7 +107,7 @@ CREATE TABLE IF NOT EXISTS `slot` (
   `id` VARCHAR(36),
   `start_time` DATETIME,
   `end_time` DATETIME,
-  `reserved` BOOLEAN,
+  `reserved` BOOLEAN DEFAULT FALSE,
   PRIMARY KEY (`id`)
 );
 
@@ -124,7 +124,7 @@ CREATE TABLE IF NOT EXISTS `request` (
   `preferred_staff` VARCHAR(36),
   `p_cancellation_reason` VARCHAR(255),
   `r_cancellation_reason` VARCHAR(255),
-  `cancelled` BOOLEAN,
+  `cancelled` BOOLEAN DEFAULT FALSE,
   `reviewer_id` VARCHAR(36),
   `appointment_type` VARCHAR(36),
   `patient_id` VARCHAR(36) NOT NULL,
@@ -167,6 +167,7 @@ CREATE TABLE IF NOT EXISTS `appointment` (
   `id` VARCHAR(36),
   `b_cancellation_reason` VARCHAR(255),
   `a_cancellation_reason` VARCHAR(255),
+  `cancelled` BOOLEAN DEFAULT FALSE,
   `booker_id` VARCHAR(36) NOT NULL,
   `availability_id` VARCHAR(36) NOT NULL,
   `request_id` VARCHAR(36) NOT NULL,
