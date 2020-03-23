87e5707bdbe29e66f74c05854eb1c8ceefab0579
Jake Love <up734426@myport.ac.uk>
2020-03-23 15:58:22 +0000

    add links to index depending on main user

--- front-end/index.php
+++ front-end/index.php
@@ -22,6 +22,50 @@ if (!isset($_SESSION['user'])) {
 
   <main>
     <?php include dirname(__FILE__) . '/../back-end/includes/error-container.inc.php';?>
+
+    <?php if ($_SESSION['user']->role_id == PATIENT_ROLE): ?>
+      <h2>Requesting Appointments</h2>
+
+      <ul>
+        <li><a href="request-appointment.php">Request an appointment</a></li>
+      </ul>
+
+      <h2>View Appointments</h2>
+
+      <ul>
+        <li><a href="appointments.php">View booked appointments</a></li>
+      </ul>
+    <?php endif ?>
+
+    <?php if ($_SESSION['user']->role_id == MEDICAL_ROLE): ?>
+      <h2>Review Appointments And Requests</h2>
+
+      <ul>
+        <li><a href="appointments.php">View requested/booked appointments</a></li>
+        <li><a href="requests.php">Review requested appointments</a></li>
+      </ul>
+
+      <h2>Add Slots To Availability</h2>
+
+      <ul>
+        <li><a href="add-availability.php">Add available slots</a></li>
+      </ul>
+    <?php endif ?>
+
+    <?php if ($_SESSION['user']->role_id == ADMINISTRATIVE_ROLE): ?>
+      <h2>Appointment Booking</h2>
+
+      <ul>
+        <li><a href="appointments.php">View requested/booked appointments</a></li>
+        <li><a href="book-appointment.php">Book reviewed appointments</a></li>
+      </ul>
+
+      <h2>Verify Accounts</h2>
+
+      <ul>
+        <li><a href="patient-accounts.php">Verify patient accounts</a></li>
+      </ul>
+    <?php endif ?>
   </main>
 </body>
 
