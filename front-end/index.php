3691942b6f8c25a2f5f9bc9cfffa489f070c71a0
Lewis Newton <lewisknewton@hotmail.com>
2020-03-23 18:26:27 +0000

    tidy up home page content

--- front-end/index.php
+++ front-end/index.php
@@ -14,58 +14,52 @@ if (!isset($_SESSION['user'])) {
 
 <head>
   <?php include dirname(__FILE__) . '/../back-end/includes/head-elements.inc.php';?>
-  <title><?= $GLOBALS['app']->title ?></title>
+  <title><?=$GLOBALS['app']->title?></title>
 </head>
 
 <body>
-  <?php include dirname(__FILE__) . '/../back-end/includes/header.inc.php'; ?>
+  <?php include dirname(__FILE__) . '/../back-end/includes/header.inc.php';?>
 
   <main>
-    <?php include dirname(__FILE__) . '/../back-end/includes/error-container.inc.php';?>
-
     <?php if ($_SESSION['user']->role_id == PATIENT_ROLE): ?>
-      <h2>Requesting Appointments</h2>
+      <h2>Appointment Booking</h2>
 
       <ul>
         <li><a href="request-appointment.php">Request an appointment</a></li>
+        <li><a href="requests.php">View your appointment booking requests</a></li>
+        <li><a href="appointments.php">View your booked appointments</a></li>
       </ul>
-
-      <h2>View Appointments</h2>
-
-      <ul>
-        <li><a href="appointments.php">View booked appointments</a></li>
-      </ul>
-    <?php endif ?>
+    <?php endif?>
 
     <?php if ($_SESSION['user']->role_id == MEDICAL_ROLE): ?>
-      <h2>Review Appointments And Requests</h2>
-
-      <ul>
-        <li><a href="appointments.php">View requested/booked appointments</a></li>
-        <li><a href="requests.php">Review requested appointments</a></li>
-      </ul>
-
-      <h2>Add Slots To Availability</h2>
+      <h2>Add Bookable Slots (Specify Availability)</h2>
 
       <ul>
         <li><a href="add-availability.php">Add available slots</a></li>
       </ul>
-    <?php endif ?>
 
-    <?php if ($_SESSION['user']->role_id == ADMINISTRATIVE_ROLE): ?>
-      <h2>Appointment Booking</h2>
+      <h2>Appointment Bookings</h2>
 
       <ul>
-        <li><a href="appointments.php">View requested/booked appointments</a></li>
-        <li><a href="book-appointment.php">Book reviewed appointments</a></li>
+        <li><a href="requests.php">View your appointment booking requests</a></li>
+        <li><a href="appointments.php">View your booked appointments</a></li>
       </ul>
+    <?php endif?>
 
-      <h2>Verify Accounts</h2>
+    <?php if ($_SESSION['user']->role_id == ADMINISTRATIVE_ROLE): ?>
+      <h2>Account Verification</h2>
 
       <ul>
         <li><a href="patient-accounts.php">Verify patient accounts</a></li>
       </ul>
-    <?php endif ?>
+
+      <h2>Appointment Bookings</h2>
+
+      <ul>
+        <li><a href="requests.php">View appointment booking requests</a></li>
+        <li><a href="appointments.php">View booked appointments</a></li>
+      </ul>
+    <?php endif?>
   </main>
 </body>
 
