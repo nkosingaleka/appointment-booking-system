87cd23a52647c808035b18c8840bc17eaa73d18c
Luke Tipler <47203384+luketipler@users.noreply.github.com>
2020-03-26 02:25:27 +0000

    change the block comment on 492 to be split so more readable

--- back-end/classes/UserManager.php
+++ back-end/classes/UserManager.php
@@ -489,7 +489,9 @@ class UserManager {
   }
 
   /**
-   * Receives an email message sent from the application to inform users of updates. Note: to work with Google Mail, the 'Less secure app access' option must be enabled.
+   * Receives an email message sent from the application to inform users of updates. 
+   *
+   * Note: to work with Google Mail, the 'Less secure app access' option must be enabled.
    *
    * @param string $userid The ID of the user's account.
    * @param string $message Message to inform users of updates.
