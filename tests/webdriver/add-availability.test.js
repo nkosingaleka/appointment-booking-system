ec929aa85155212d916b455f8643551c99e9be38
Alexander-Portland <up896020@myport.ac.uk>
2020-03-26 18:20:51 +0000

    add test scripts for the add-availability page

--- tests/webdriver/add-availability.test.js
+++ tests/webdriver/add-availability.test.js
@@ -36,3 +36,12 @@ afterAll(async () => {
     expect(await driver.findElement(By.css('.success-message > ul')).getText()).toBe('New slots have been successfully added.\nYour availability has been successfully added.');
     await driver.get(`${url}/front-end/add-availability.php`);
   });
+
+  test('this test will try to book an invalid time where the end date occurs before the start date', async () => {
+  
+    await driver.findElement(By.id('start_time')).sendKeys('2001-06-02T16:00');
+    await driver.findElement(By.id('end_time')).sendKeys('2000-06-02T17:00');
+    await driver.findElement(By.id("add")).click();
+
+    expect(await driver.findElement(By.css('.error-message > ul')).getText()).toBe('Please do not enter start and end times in the past.\nSorry, your times were in an incorrect format. Please check your input and try again.');
+  });
