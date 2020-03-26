1dfa13023079da0e19f98cdaa45206642226ae5d
Alexander-Portland <up896020@myport.ac.uk>
2020-03-26 17:27:32 +0000

    add a test script for add-availability page

--- tests/webdriver/add-availability.test.js
+++ tests/webdriver/add-availability.test.js
@@ -8,6 +8,10 @@ const By = core.By;
 beforeAll(async () => {
   try {
     await driver.get(`${url}/front-end/login.php`);
+    await driver.findElement(By.id('email')).sendKeys('ms1@test.com');
+    await driver.findElement(By.id('password')).sendKeys('test123');
+    await driver.findElement(By.id('login')).click();
+    await driver.get(`${url}/front-end/add-availability.php`);
   } catch (err) {
     console.error(err);
   }
@@ -21,22 +25,14 @@ afterAll(async () => {
   }
 }, timeout);
 
-test('This test will log the user in and test the add button on the availability page to test for absent input error message', async () => {
-    await driver.findElement(By.id('email')).sendKeys('ms1@test.com');
-    await driver.findElement(By.id('password')).sendKeys('test123');
-    await driver.findElement(By.id('login')).click();
-    await driver.get(`${url}/front-end/add-availability.php`);
-    await driver.findElement(By.id("add")).click();
-  })
-
-test('This test will log the user in and test the add button on the availability page to test for absent input error message', async () => {
-    await driver.findElement(By.id('start_time')).sendKeys('03/15/2020 07:00 AM');
-    await driver.findElement(By.id("add")).click();
-})
-
-test('This test will log the user in and test the add button on the availability page to test for absent input error message', async () => {
-    await driver.findElement(By.id('start_time')).sendKeys('03/15/2020 07:00 AM');
-    await driver.findElement(By.id('end_time')).sendKeys('03/15/2020 08:00 PM');
-    await driver.findElement(By.id("add")).click();
-})
   //await driver.findElement(By.id('')).sendKeys('');
+
+  test('this test will successfully select a a time period and book an availible time slot', async () => {
+  
+    await driver.findElement(By.id('start_time')).sendKeys('2020-06-02T16:00');
+    await driver.findElement(By.id('end_time')).sendKeys('2020-06-02T17:00');
+    await driver.findElement(By.id("add")).click();
+  
+    expect(await driver.findElement(By.css('.success-message > ul')).getText()).toBe('New slots have been successfully added.\nYour availability has been successfully added.');
+    await driver.get(`${url}/front-end/add-availability.php`);
+  });
